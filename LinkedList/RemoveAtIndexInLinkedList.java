0f798829bb5d0ab6847b07757e5f1e82ff57619a
Anirudh Singh <anirudh.singh@pojolabs.in>
2021-06-18 23:23:45 +0530

    adds new linked list program to remove an elment at an index

---
+++ LinkedList/RemoveAtIndexInLinkedList.java
@@ -0,0 +1,232 @@
+package LinkedList;
+
+public class RemoveAtIndexInLinkedList {
+
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        int size() {
+            return size;
+        }
+
+        void display() {
+            Node tempHead = head;
+            while (tempHead != null) {
+                System.out.print(tempHead.data + " ");
+                tempHead = tempHead.next;
+            }
+            System.out.println();
+        }
+
+        void removeFirst() {
+            if (size == 0)
+                return;
+            else if (size == 1) {
+                head = tail = null;
+                size = 0;
+            } else {
+                head = head.next;
+                size--;
+            }
+            // garbage collector job
+            // // Node tempHead = head;
+            // // tempHead = head.next;
+            // // head = null;
+            // // head = tempHead;
+        }
+
+        int getFirst() {
+            if (size <= 0) {
+                return -1;
+            }
+            return head.data;
+        }
+
+        int getLast() {
+            if (size <= 0) {
+                return -1;
+            }
+            return tail.data;
+        }
+
+        int getAtIndex(int idx) {
+            if (size <= 0) {
+                System.out.println("List is empty");
+                return -1;
+            } else if (size <= idx || idx < 0) {
+                System.out.println("Invalid arguments");
+                return -1;
+            } else {
+                int index = 0;
+                Node tempNode = head;
+                while (index < idx) {
+                    tempNode = tempNode.next;
+                    index++;
+                }
+
+                return tempNode.data;
+            }
+        }
+
+        void addFirst(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = head;
+            head = temp;
+
+            if (size == 0) {
+                tail = temp;
+            }
+
+            size++;
+        }
+
+        void addAtIndex(int idx, int val) {
+            Node temp = new Node();
+            temp.data = val;
+
+            if (idx < 0 || idx > size) {
+                System.out.println("invalid index");
+                return;
+            }
+            // first/last and empty
+            else if (size == 0 && idx == 0) {
+                head = tail = temp;
+            }
+            // first
+            else if (idx == 0 && size > 0) {
+                addFirst(val);
+            }
+            // last
+            else if (idx == size) {
+                addLast(val);
+            } else {
+                Node tempNode = head;
+                for (int i = 0; i < idx; i++) {
+                    tempNode = tempNode.next;
+                }
+                temp.next = tempNode.next;
+                tempNode.next = temp;
+                size++;
+            }
+        }
+
+        void removeLast() {
+            if (size == 0) {
+                System.out.println("List is empty, cannot remove an element");
+                return;
+            } else if (size == 1) {
+                tail = head = null;
+                size = 0;
+            } else {
+                Node temp = head;
+                for (int i = 0; i < size - 2; i++) {
+                    temp = temp.next;
+                }
+                tail = temp;
+                temp.next = null;
+                size--;
+            }
+        }
+
+        void reverse() {
+            if (size == 0 || size == 1)
+                return;
+
+            Node prev = null;
+            Node curr = head;
+            while (curr != null) {
+                Node next = curr.next;
+
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            // swap head with tail
+            Node temp = head;
+            head = tail;
+            tail = temp;
+        }
+
+        void removeAtIndex(int idx){
+            if(size == 0){
+                System.out.println("list is empty");
+            }
+            else if(idx >= size){
+                System.out.println("invalid index");
+            }
+            else if(idx == 0){
+                removeFirst();
+            }
+            else if(idx == size -1){
+                removeLast();
+            }
+            else {
+                Node temp = head;
+                for(int i = 0 ; i<idx-1; i++){
+                    temp = temp.next;
+                }
+                temp.next = temp.next.next;
+                size--;
+            }
+        }
+
+        // private Node getNodeAt(int idx) {
+        // Node temp = head;
+        // for (int i = 0; i < idx; i++) {
+        // temp = temp.next;
+        // }
+
+        // return temp;
+        // }
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.addLast(10);
+        ll.addLast(20);
+        ll.addLast(30);
+        ll.addLast(40);
+        ll.addLast(50);
+        ll.addLast(60);
+        ll.display();
+        ll.removeAtIndex(4);
+        ll.display();
+        
+        // System.out.println("Reveresed list:");
+        // ll.reverse();
+        // ll.display();
+        // System.out.println("size of linked list: " + ll.size());
+        // ll.removeLast();
+        // ll.removeFirst();
+        // ll.removeFirst();
+        // ll.addFirst(5);
+        // ll.addFirst(51);
+        // ll.addAtIndex(2, 88);
+        // System.out.println("Value at index 1: " + ll.getAtIndex(3));
+
+    }
+}
